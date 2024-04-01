908ac284b4f2c34dc420f80e771b63def8c47ebf
강경규 <qwzx16@naver.com>
2024-04-01 16:17:46 +0900

    CHORE: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();

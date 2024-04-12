43a146e33ed1666e33989c0b416149a21b80e870
강경규 <qwzx16@naver.com>
2024-04-12 17:02:02 +0900

    FEAT: swagger & dto 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,39 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserJoinRequestDTO } from './dto/UserJoinRequestDTO';
+import { UserLoginRequestDTO } from './dto/UserLoginRequestDTO';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @HttpCode(HttpStatus.OK)
   @Post('/join')
+  @ApiOperation({
+    summary: '회원가입',
+    description: '사용자 정보를 추가합니다.',
+  })
+  @ApiBody({ type: UserJoinRequestDTO })
+  @ApiResponse({ status: 201, description: '회원가입에 성공하였습니다' })
+  @ApiResponse({ status: 404, description: '회원가입에 실패하였습니다' })
   createUser(@Body() userInfo) {
     return this.usersService.registerUser(userInfo);
+  }
 
-    /**
-  {
-    "email": "user1@example.com",
-    "password": "password123",
-    "name": "User One",
-    "address": "123 Main Street, City, Country",
-    "phoneNumber": "123-456-7890"
-  }
-    */
-  }
   @Post('/login')
+  @ApiOperation({
+    summary: '로그인',
+    description: '사용자를 인증 후 로그인 여부를 반환합니다.',
+  })
+  @ApiBody({ type: UserLoginRequestDTO })
+  @ApiResponse({ status: 200, description: '로그인 성공' })
+  @ApiResponse({ status: 400, description: '존재하지 않는 이메일 입니다.' })
+  @ApiResponse({ status: 401, description: '비밀번호가 일치하지 않습니다.' })
   login(@Body() userInfo) {
     return this.usersService.authenticateUser(
       userInfo.email,
       userInfo.password,
     );
-    /**
-  {
-    "email": "user1@example.com",
-    "password": "password123",
-  }
-     */
   }
 }

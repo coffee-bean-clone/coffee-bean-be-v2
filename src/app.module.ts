38d2b30df5675a3dc0c7bf0e566339636448b2ed
강경규 <qwzx16@naver.com>
2024-04-12 17:00:58 +0900

    REFACTOR: test용 코드 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 import { ProductModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
@@ -13,7 +12,6 @@ import { CategoryModule } from './category/category.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
-    CatsModule,
     ProductModule,
     UsersModule,
     CategoryModule,

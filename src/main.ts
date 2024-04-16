ce41658069e2b061dd15a8229b2ca001ba42fffe
강경규 <qwzx16@naver.com>
2024-04-16 14:46:25 +0900

    add: 의존성 설치, main.ts cookie-parser 사용

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('coffee-bean-API')
+    .setDescription('description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('tag')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  // const config2 = new DocumentBuilder()
+  //   .setTitle('coffee-bean-API')
+  //   .setDescription('description')
+  //   .setVersion('1.0')
+  //   .addTag('tag')
+  //   .build();
+  // const document2 = SwaggerModule.createDocument(app, config2);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
     origin: true,
@@ -18,6 +26,7 @@ async function bootstrap() {
     exposedHeaders: ['Authorization'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
+  app.use(cookieParser);
   await app.listen(process.env.PORT);
 }
 bootstrap();

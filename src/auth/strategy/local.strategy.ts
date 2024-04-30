b248e313fd6e43c64696afe6a77f0178a6c5da81
강경규 <qwzx16@naver.com>
2024-04-30 20:59:53 +0900

    FIX: dto 수정

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -2,7 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-import { UserLoginRequestDTO } from 'src/users/dto/request/UserLoginRequestDTO';
+import { REQ } from 'src/users/dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
@@ -11,7 +11,7 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   }
 
   async validate(email: string, password: string): Promise<any> {
-    const userLoginRequestDTO: UserLoginRequestDTO = {
+    const userLoginRequestDTO: REQ.UserLoginRequestDTO = {
       email: email,
       password: password,
     };
